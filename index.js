4cb0f0e7ba6e1a9771dbe76507bdfd8b2a1381e7
Joaquín Ormaechea <joaquinormaechea@gmail.com>
2022-08-02 12:36:08 -0300

    Added typings, removed node12 support and added node16 support

--- index.js
+++ index.js
@@ -2,6 +2,11 @@
 
 const { APITest } = require('./lib');
 
+/**
+ * @param {import('@janiscommerce/api').API} APIClass
+ * @param {string} endpoint
+ * @param {import('./lib/api-test').TestRule[]} rules
+ */
 module.exports = (APIClass, endpoint, rules) => {
 	const apiTest = new APITest(APIClass, endpoint, rules);
 	apiTest.test();
