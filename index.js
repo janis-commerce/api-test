2e5d255abeba9734301ea4f4249401fe7503ea45
Joaquín Ormaechea <joaquinormaechea@gmail.com>
2025-03-31 18:20:41 -0300

    Fixed entry point so it can receive a function or an object with a handler

--- index.js
+++ index.js
@@ -3,11 +3,11 @@
 const { APITest } = require('./lib');
 
 /**
- * @param {import('./lib/api-test').ApiHandler} apiHandler
+ * @param {import('./lib/api-test').ApiHandler|{ handler:import('./lib/api-test').ApiHandler }} apiHandler
  * @param {string} endpoint
  * @param {import('./lib/api-test').TestRule[]} rules
  */
 module.exports = (apiHandler, endpoint, rules) => {
-	const apiTest = new APITest(apiHandler, endpoint, rules);
+	const apiTest = new APITest(typeof apiHandler === 'function' ? apiHandler : apiHandler.handler, endpoint, rules);
 	apiTest.test();
 };
